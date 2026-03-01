bc65100d66ac5c2f7b926d483f8b949757d2eff1
Rupeshkumar4 <geniusrupeshkumar@gmail.com>
2026-03-01 13:04:06 +0530

    Inital end of session 2

---
+++ API/entities/AppUser.cs
@@ -0,0 +1,9 @@
+
+namespace API.entities;
+
+public class AppUser
+{
+    public  string Id { get; set; }=Guid.NewGuid().ToString();
+    public required string UserName { get; set; }
+    public required string Email { get; set; }
+}
