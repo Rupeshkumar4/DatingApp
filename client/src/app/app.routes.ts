654b96b6ce25103bdccf92075bf564bab03a9f35
Rupeshkumar4 <geniusrupeshkumar@gmail.com>
2026-05-30 23:45:56 +0530

    Added Route RouteGuard And animiated link withViewTransitions

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { MemberList } from '../features/members/member-list/member-list';
+import { Messages } from '../features/messages/messages';
+import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
+import { Home } from '../features/home/home';
+import { Lists } from '../features/lists/lists';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Home},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MemberList },
+            { path: 'members/:id', component: MemberDetailed },
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },
+        ]
+    },
+    {path:'**', component:Home}
+
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,25 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, Signal, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
-import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
-import { Home } from "../features/home/home";
+// import { HttpClient } from '@angular/common/http';
+// import { Component, inject, OnInit, Signal, signal } from '@angular/core';
+// import { Router, RouterOutlet } from '@angular/router';
+// import { lastValueFrom } from 'rxjs';
+// import { Nav } from "../layout/nav/nav";
+// import { AccountService } from '../core/services/account-service';
+// import { User } from '../types/user';
+// import { NgClass } from '@angular/common';
 
-@Component({
-  selector: 'app-root',
-  imports: [Nav, Home],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App implements OnInit {
 
-    private accountService = inject(AccountService);
+// @Component({
+//   selector: 'app-root',
+//   imports: [Nav, RouterOutlet, NgClass],
+//   templateUrl: './app.html',
+//   styleUrl: './app.css'
+// })
+// export class App implements OnInit {
+
+//   private accountService = inject(AccountService);
   
-  private http = inject(HttpClient); // constructor(private http: HttpClient){}
-  protected readonly title = signal('Dating App');
-  protected members = signal<User[]>([]);
+  // private http = inject(HttpClient); // constructor(private http: HttpClient){}
+  // protected readonly title = signal('Dating App');
+  // protected members = signal<User[]>([]);
+  // protected router = inject(Router);
 
   // oder dont matters of functions
   // ngOnInit():void{
@@ -31,27 +33,46 @@ export class App implements OnInit {
   //   })
   // }; 
 
-   async ngOnInit() {
-    this.members.set(await this.getMembers())
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
+  //  async ngOnInit() {
+  //   this.members.set(await this.getMembers())
+  //   this.setCurrentUser();
+  // }
 
-   setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
+  // This code is moved to init service and called in app initializer provider in app config
+  //  setCurrentUser() {
+  //   const userString = localStorage.getItem('user');
+  //   if (!userString) return;
+  //   const user = JSON.parse(userString);
+  //   this.accountService.currentUser.set(user);
+  // }
 
-  async getMembers(){
-    try{
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    }
-    catch(error){
-      console.log(error);
-      throw error;
-    }
-  }
+//   async getMembers(){
+//     try{
+//       return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
+//     }
+//     catch(error){
+//       console.log(error);
+//       throw error;
+//     }
+//   }
 
+// }
+
+
+// This code after initService and above on is before initService
+
+import { Component, inject } from '@angular/core';
+import { Nav } from "../layout/nav/nav";
+import { Router, RouterOutlet } from '@angular/router';
+import { NgClass } from "@angular/common";
+@Component({
+  selector: 'app-root',
+  imports: [Nav, RouterOutlet, NgClass],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+
+  private router = inject(Router);
 }
